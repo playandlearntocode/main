47ec3c906df5007b81b3e2aee4dc348e63f8ae63
playandlearntocode.com <playandlearntocode@gmail.com>
2021-03-26 18:04:44 +0100

    Small update

--- nlp/skip_gram/src/classes/config.py
+++ nlp/skip_gram/src/classes/config.py
@@ -5,7 +5,7 @@ class Config:
     HIDDEN_LENGTH = 8  # embedding size
     OUTPUT_LENGTH = 17  # same dimensionallity as input
 
-    WINDOW_LENGTH = 1
+    WINDOW_LENGTH = 1 # one word to the left and one word to the right
 
     # FULL VOCABULARY:
     VOCABULARY = ['.', 'how', 'are', 'you', 'what', 'is', 'your', 'name', 'where', 'have', 'been', 'go', 'doing',
